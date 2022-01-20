c9e6b182c4c8f19d0d4d7cbb0543440369bfa71f
The Code Genie <ryanlangston1993@gmail.com>
2022-01-20 17:08:51 +0000

    close #1

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailForm = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "thecodegenieuk@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have a message ".$name".\n\n" .$message;
+    
+    mail($mailTo, $name, $txt, $headers);
+
+    header("Location: index.html?MessageSent");
+}
+
+
+?>
